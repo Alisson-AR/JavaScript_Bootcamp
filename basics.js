6902a564ff6d56b68180a1e95ee5a61635081b23
Alisson Rosa <105574563+Alisson-AR@users.noreply.github.com>
2022-08-01 23:58:25 -0300

    Basic JS interactions

---
+++ basics.js
@@ -0,0 +1,35 @@
+//Multliplicação 
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(2, 4));
+
+//Tamanho da String
+
+function contaCaractere(str) {
+    let contaCaractere = 'Alisson';
+    return str.length
+} 
+
+console.log (contaCaractere("Alisson"));
+
+//Retornar em caixa alta
+
+function gritarNome(nome) {
+    let gritarNome = 'Alissooon';
+    return nome.toUpperCase();
+}
+
+console.log (gritarNome("Alissooon"));
+
+
+//Retornar em caixa baixa
+
+function caixaBaixa(nome) {
+    let caixaBaixa = 'Alissooon';
+    return nome.toLowerCase();
+}
+
+console.log (caixaBaixa("Alissooon"));
