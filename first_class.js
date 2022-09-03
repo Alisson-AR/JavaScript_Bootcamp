8252ba4d5cda769dc8cdf945a5cf037eec264540
Alisson Rosa <alisson.aln.rosa@gmail.com>
2022-09-02 23:26:39 -0300

    Foram realizadas melhorias

--- first_class.js
+++ first_class.js
@@ -4,16 +4,17 @@
     - Valor médio do consumo do carro;
     - Distância da viagem; 
 
-    Por fim imprimir no console o valor que será gasto em combustivel para a viagem. */
+Por fim imprimir no console o valor que será gasto em combustivel para a viagem. */
 
-    console.log('CALCULO DE GASTO DO COMBUSTIVEL');
+    console.log('CALCULO DE GASTO DO COMBUSTIVEL:');
     let gasolina = 6.98;
     let consumo = 10; //Faz 10km por litro
-    let km = 460; //Km que será percorrido
+    let km = 460; //Kms que serão percorridos
 
     const vlitro = ((km/consumo) * gasolina);
      
-    console.log("SERA NECESSARIO DESENBOLSAR R$", vlitro, "\nPARA A SUA VIAGEM");
+    console.log("SERA NECESSARIO DESENBOLSAR R$", vlitro.toFixed(2), "\nPARA REALIZAR A VIAGEM");
 
-
+/*Observar o uso do toFixed que possibilita definir o número desejado de casas decimais
+tornando o vlitro em uma string*/
 
